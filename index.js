c553fceff7f4c94a37ad8fd8419afeb79bb99476
BPgrouptech <bpgroup.tech@hotmail.com>
2026-04-24 14:07:35 -0600

    remove duplicated cors

--- index.js
+++ index.js
@@ -11,12 +11,6 @@ const pool = require("./db");
 
 const app = express();
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "http://localhost:5174",
-  "https://bpgroup-panel.vercel.app",
-  process.env.FRONTEND_URL
-].filter(Boolean);
 
 const allowedOrigins = [
   "http://localhost:5173",
